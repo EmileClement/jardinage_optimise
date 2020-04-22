63cf093c155b0c31f7952f16895dc8d52258ecc5
Leo <leopold.clement@laposte.net>
2020-04-22 22:10:05 +0200

    On a les jardins vide
    Signed-off-by: Leo <leopold.clement@laposte.net>

--- Jardinier.py
+++ Jardinier.py
@@ -4,9 +4,6 @@ Created on Mon Apr 20 11:51:41 2020
 
 @author: Leopold
 """
-class Climat():
-    pass
-
 class Jardin():
     def __init__(self, len_x: int, len_y: int):
         self.emplacement = [[Emplacement(self) for i in range(len_x)] for i in range(len_y)]
@@ -14,10 +11,15 @@ class Jardin():
 class Emplacement():
     def __init__(self, jardin):
         self.jardin = jardin
+        self.calendrier = [Jachere() for i in range(52)]
         
+class Occupant():
+    def __init__(self):
+        assert 0, "Not implemeted"
 
+class Jachere(Occupant):
+    def __init__(self):
+        pass
+        
 class Plante():
     pass
-
-
-    
